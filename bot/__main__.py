c662abcaf80a8cce6244bd0adaf3a1ed64fb36d7
DEADSECURITY-INDIA <106884564+DEADSECURITY-INDIA@users.noreply.github.com>
2022-08-10 03:46:35 -0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -54,8 +54,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/arshsisodiya/helios-mirror")
-    buttons.buildbutton("Support Group", "https://t.me/mirrorsociety")
+    buttons.buildbutton("Repo", "https://github.com/DEADSECURITY-INDIA/DARKY-LEECH-BOT")
+    buttons.buildbutton("Support Group", "https://t.me/DARKY_LEECH_SUPPORT")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
